3ccb29a46a59e15e52973633b72cbd9d48e69ca4
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-17 11:23:47 -0600

    created structure for the react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import './App';
+
+
+import Header from "./components/header";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
