3dce8b266ea4f6fec5d91334314ac8b4b7699186
Shital Chaudhary <shitalchau10@gmail.com>
2021-11-19 13:06:24 -0600

    add thumbnails to projects

---
+++ src/pages/Resume.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Layout, Typography} from "antd";
+const { Content } = Layout;
+const { Title } = Typography;
+
+
+const Resume = () => {
+    return(
+        <Content>
+            <Title level={2}>Resume</Title>
+        </Content>
+    );
+}
+
+export default Resume;
